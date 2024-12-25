fe2fd1389b6d407eb3087042fd0a83ea0468d661
lmathias2708 <65302116+lmathias2708@users.noreply.github.com>
2024-12-25 18:42:29 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -8,12 +8,19 @@ const hoursA = document.getElementById('hoursa');
 const minutesA = document.getElementById('mina');
 const secondsA = document.getElementById('seca');
 
+const daysN = document.getElementById('daysn');
+const hoursN = document.getElementById('hoursn');
+const minutesN = document.getElementById('minn');
+const secondsN = document.getElementById('secn');
+
 const conhecemos = "8 nov 2024 23:59:00";
 const amor = "27 nov 2024 04:09:00";
+const namoro = "13 dec 2024 16:30:00";
 
 function countup() {
     const conhecemosDate = new Date(conhecemos);
     const amorDate = new Date(amor);
+    const namoroDate = new Date(namoro);
 
     const currentDate = new Date();
 
@@ -29,6 +36,12 @@ function countup() {
     var minutesa = Math.floor(totalSecondsa / 60) % 60;
     var secondsa = Math.floor(totalSecondsa) % 60;
 
+    const totalSecondsn = (currentDate - namoroDate) / 1000;
+    var daysn = Math.floor(totalSecondsn / 3600 / 24);
+    var hoursn = Math.floor(totalSecondsn / 3600) % 24;
+    var minutesn = Math.floor(totalSecondsn / 60) % 60;
+    var secondsn = Math.floor(totalSecondsn) % 60;
+
     daysC.innerHTML = daysc;
     hoursC.innerHTML = formatTime(hoursc);
     minutesC.innerHTML = formatTime(minutesc);
@@ -40,6 +53,12 @@ function countup() {
     minutesA.innerHTML = formatTime(minutesa);
     secondsA.innerHTML = formatTime(secondsa);
     console.log(daysa, hoursa);
+
+    daysN.innerHTML = daysn;
+    hoursN.innerHTML = formatTime(hoursn);
+    minutesN.innerHTML = formatTime(minutesn);
+    secondsN.innerHTML = formatTime(secondsn);
+    console.log(daysn, hoursn);
     
 };
 
