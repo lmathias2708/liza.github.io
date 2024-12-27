d0594ad594f6bf14c8767960ddb176d7c6397d05
lmathias2708 <65302116+lmathias2708@users.noreply.github.com>
2024-12-27 11:31:21 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const daysC = document.getElementById('daysc');
 const hoursC = document.getElementById('hoursc');
 const minutesC = document.getElementById('minc');
@@ -8,6 +9,11 @@ const hoursA = document.getElementById('hoursa');
 const minutesA = document.getElementById('mina');
 const secondsA = document.getElementById('seca');
 
+const daysF = document.getElementById('daysf');
+const hoursF = document.getElementById('hoursf');
+const minutesF = document.getElementById('minf');
+const secondsF = document.getElementById('secf');
+
 const daysN = document.getElementById('daysn');
 const hoursN = document.getElementById('hoursn');
 const minutesN = document.getElementById('minn');
@@ -15,11 +21,13 @@ const secondsN = document.getElementById('secn');
 
 const conhecemos = "8 nov 2024 23:59:00";
 const amor = "27 nov 2024 04:09:00";
+const ficamos = "1 dec 2024 21:30:00";
 const namoro = "13 dec 2024 16:30:00";
 
 function countup() {
     const conhecemosDate = new Date(conhecemos);
     const amorDate = new Date(amor);
+    const ficamosDate = new Date(ficamos);
     const namoroDate = new Date(namoro);
 
     const currentDate = new Date();
@@ -35,6 +43,12 @@ function countup() {
     var hoursa = Math.floor(totalSecondsa / 3600) % 24;
     var minutesa = Math.floor(totalSecondsa / 60) % 60;
     var secondsa = Math.floor(totalSecondsa) % 60;
+
+    const totalSecondsf = (currentDate - ficamosDate) / 1000;
+    var daysf = Math.floor(totalSecondsf / 3600 / 24);
+    var hoursf = Math.floor(totalSecondsf / 3600) % 24;
+    var minutesf = Math.floor(totalSecondsf / 60) % 60;
+    var secondsf = Math.floor(totalSecondsf) % 60;
 
     const totalSecondsn = (currentDate - namoroDate) / 1000;
     var daysn = Math.floor(totalSecondsn / 3600 / 24);
@@ -54,6 +68,12 @@ function countup() {
     secondsA.innerHTML = formatTime(secondsa);
     console.log(daysa, hoursa);
 
+    daysF.innerHTML = daysf;
+    hoursF.innerHTML = formatTime(hoursf);
+    minutesF.innerHTML = formatTime(minutesf);
+    secondsF.innerHTML = formatTime(secondsf);
+    console.log(daysf, hoursf);
+
     daysN.innerHTML = daysn;
     hoursN.innerHTML = formatTime(hoursn);
     minutesN.innerHTML = formatTime(minutesn);
@@ -65,6 +85,7 @@ function countup() {
 function formatTime(time) {
     return time < 10 ? `0${time}` : time;
 }
+
 
 countup();
 
